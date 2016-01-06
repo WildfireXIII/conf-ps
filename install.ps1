7c604f3469088ec82105e9d20d810865e38c847e
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-01-06 01:21:16 -0500

    Took out unnecessary parts from quote

--- install.ps1
+++ install.ps1
@@ -1,2 +1,2 @@
-$username = Read-Host -Prompt "Please enter user account name: "
+$username = Read-Host -Prompt "Please enter user account name"
 copy "$env:PKG_DIR\conf-ps\Microsoft.PowerShell_profile.ps1" "C:\Users\$username\Documents\WindowsPowerShell"
